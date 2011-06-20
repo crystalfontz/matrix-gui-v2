572a679fb2154191a7a4aae7797d3591e4a339b0
Thomas Potthast <tpotthast@ti.com>
2011-06-20 14:56:12 -0500

    added .desktop support and cache clearing

--- server.js
+++ server.js
@@ -7,13 +7,22 @@ var sys = require("sys"),
     loader = require("./lib/load_static_file"),
     applist = require("./lib/applist"),
     appLauncher = require("./lib/apps"),
-    io = require("./lib/socket.io/lib/socket.io/");
+    io = require("./lib/socket.io/lib/socket.io/"),
+    mUtils = require("./lib/matrixutils.js"),
+    pageBuilder = require("./lib/pageBuilder.js");
 
 var server = http.createServer(function(request, response) {  
-	var uri = url.parse(request.url).pathname;  
-	if(applist.isAppListRequest(uri)) {  
-		applist.generateAppList(uri, response);  
-	}else {  
+	var uri = url.parse(request.url).pathname;
+//	console.log("new request for uri: " + uri);  
+	if(mUtils.isAppListRequest(uri)) {  
+		applist.generateAppList(uri, response);
+	}else if(mUtils.isAppDescriptionRequest(uri)){
+		pageBuilder.createAppPage(uri, response); 
+	}else if(mUtils.isAppIconRequest(uri)){
+		loader.loadIcon(uri, response);
+	}else if(mUtils.isCacheClearRequest(uri)){
+		mUtils.clearCache(response);
+	}else{  
 		loader.load_static_file(uri, response);  
 	}  
 });
